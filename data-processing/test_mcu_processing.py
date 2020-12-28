940b0ba394e385c96f4a7df276c98425aa50a597
Paolo <info@paoloinfante.it>
2020-12-28 16:38:59 +0100

    added averaging

--- data-processing/test_mcu_processing.py
+++ data-processing/test_mcu_processing.py
@@ -2,6 +2,8 @@ import struct
 import serial
 from sys import stderr
 import signal
+from typing import Tuple
+import math
 
 
 # This script is supposed to work with a modified version of the firmware by Esben Rossel
@@ -23,6 +25,8 @@ import signal
 
 SH = 200
 ICG = 100000
+averaging = True
+AVGS_NUM = 5
 
 continuous = True
 intheloop = False
@@ -45,7 +49,7 @@ def rx_parse_data(data: bytes) -> int:
     return res
 
 
-def request_data():
+def request_data() -> int:
     try:
         with serial.Serial('COM8', baudrate=115200, timeout=3) as ccd:
             tx_dt = tx_data(sh=SH, icg=ICG, integrations=1, single=True)
@@ -56,10 +60,27 @@ def request_data():
             recv = ccd.read(8)
 
         rx_data = rx_parse_data(recv)
-        print(f"Shadow edge: {str(rx_data)}px")
+        return rx_data
     except Exception as e:
         print("An error occurred", file=stderr)
         print(e, file=stderr)
+
+
+def request_avgs() -> Tuple[float,float]:
+    averages = []
+    for i in range(0, AVGS_NUM):
+        averages.append(request_data())
+    average_res = 0.0
+    for avg_measure in averages:
+        average_res += avg_measure
+    average_res /= AVGS_NUM
+
+    variance = 0.0
+    for i in range(0, AVGS_NUM):
+        variance += (averages[i] - average_res)**2
+    variance /= (AVGS_NUM-1)
+
+    return average_res, math.sqrt(variance)/math.sqrt(AVGS_NUM)
 
 
 def update_params():
@@ -77,11 +98,31 @@ def update_params():
         print("Number not recognized")
 
 
+def update_averaging():
+    global averaging, AVGS_NUM
+    newavg = input(f"Enable averaging? (Current: {'yes' if averaging is True else 'no'}) (y/n): ")
+    if newavg == "y":
+        averaging = True
+    elif newavg == "n":
+        averaging = False
+    else:
+        print("Input not recognized")
+
+    newavgnum = input(f"How many averages (each one takes ~1.5 sec)? ({AVGS_NUM}): ")
+    if newavgnum.isnumeric():
+        AVGS_NUM = int(newavgnum)
+    else:
+        print("Number not recognized")
+
+
 def show_help():
+    print(f"Current values: SH={str(SH)} ICG={str(ICG)} Averaging={'yes' if averaging is True else 'no'} Averages={str(AVGS_NUM)}")
+    print("")
     print("Available commands:")
     print("\tr -> read value (default)")
     print("\tc -> continuous reading")
     print("\ts -> set sh, icg")
+    print("\ta -> set averaging")
     print("\th -> print this help")
     print("\tq -> quit")
     print("")
@@ -91,14 +132,18 @@ def main():
     signal.signal(signal.SIGINT, termination)
     signal.signal(signal.SIGTERM, termination)
 
-    print(f"Defaults: SH={str(SH)} ICG={str(ICG)}")
     print()
     try:
         show_help()
         while 1:
             command = input("Enter the action: ")
             if len(command) == 0 or command == "r":
-                request_data()
+                if averaging is True:
+                    avgs, err = request_avgs()
+                    print("Shadow edge: %.1f +- %.1f px" % (avgs, err))
+                else:
+                    res = request_data()
+                    print(f"Shadow edge: {str(res)}px")
             elif command == "h":
                 show_help()
             elif command == "c":
@@ -107,12 +152,15 @@ def main():
                 intheloop = True
 
                 while continuous:
-                    request_data()
+                    res = request_data()
+                    print(f"Shadow edge: {str(res)}px")
 
                 print("Readings stopped")
                 intheloop = False
             elif command == "s":
                 update_params()
+            elif command == "a":
+                update_averaging()
             elif command == "q":
                 exit(0)
             else:
